2d818c72a75992c04fa40e7fc91b864b9eb66de3
Jorge Romero Burdalo <8345@iesluisbraille.es>
2022-02-07 12:05:23 +0100

    Versión 4

--- europrueba.py
+++ europrueba.py
@@ -16,3 +16,17 @@ print(f"{numeros[6]} - {numeros[16]} - {numeros[26]} - {numeros[36]} - {numeros[
 print(f"{numeros[7]} - {numeros[17]} - {numeros[27]} - {numeros[37]} - {numeros[47]}")  
 print(f"{numeros[8]} - {numeros[18]} - {numeros[28]} - {numeros[38]} - {numeros[48]}")  
 print(f"{numeros[9]} - {numeros[19]} - {numeros[29]} - {numeros[39]} - {numeros[49]}")
+
+for numero in range(5):
+  n1=int(input("Introduce el número: "))
+  numeros[0+n1-1]= "X"
+  print(f"{numeros[0]} - {numeros[10]} - {numeros[20]} - {numeros[30]} - {numeros[40]}")
+  print(f"{numeros[1]} - {numeros[11]} - {numeros[21]} - {numeros[31]} - {numeros[41]}")
+  print(f"{numeros[2]} - {numeros[12]} - {numeros[22]} - {numeros[32]} - {numeros[42]}")  
+  print(f"{numeros[3]} - {numeros[13]} - {numeros[23]} - {numeros[33]} - {numeros[43]}")
+  print(f"{numeros[4]} - {numeros[14]} - {numeros[24]} - {numeros[34]} - {numeros[44]}")  
+  print(f"{numeros[5]} - {numeros[15]} - {numeros[25]} - {numeros[35]} - {numeros[45]}")  
+  print(f"{numeros[6]} - {numeros[16]} - {numeros[26]} - {numeros[36]} - {numeros[46]}")  
+  print(f"{numeros[7]} - {numeros[17]} - {numeros[27]} - {numeros[37]} - {numeros[47]}")  
+  print(f"{numeros[8]} - {numeros[18]} - {numeros[28]} - {numeros[38]} - {numeros[48]}")  
+  print(f"{numeros[9]} - {numeros[19]} - {numeros[29]} - {numeros[39]} - {numeros[49]}") 
